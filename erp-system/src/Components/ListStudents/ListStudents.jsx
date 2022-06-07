1d69d98c669f4a0936f3bd70c90ccb481a4dbb73
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-06-07 23:03:33 +0530

    Filter data added

--- erp-system/src/Components/ListStudents/ListStudents.jsx
+++ erp-system/src/Components/ListStudents/ListStudents.jsx
@@ -5,17 +5,60 @@ import React, { useEffect, useState } from 'react'
 export default function ListStudents() {
 
 const[data , setData] = useState([])
+const[name1 , setName1] = useState("")
+const[roll1 , setRoll1] = useState("")
+const[contact1 , setContact1] = useState("")
+
+// console.log("name1111" , name1 , roll1 , contact1)
+
+// var filterDataOne ;
+// var filterData ;
 
     useEffect(() => {
-        axios.get("http://localhost:8080/student").then((res) => setData(res.data))
+        axios.get("http://localhost:8080/student").then((res) => {setData(res.data)} )
+        // const filterData = data.filter((e) => e.role === "Student")
+
     },[])
+    const filterData = data.filter((e) => e.role === "Student")
     console.log("dataa" , data)
 
-    const filterData = data.filter((e) => e.role === "Student")
+    // const filterforStudent = data.filter((e) => e.role === "Student")
+
+    
+
+
+    // useEffect(() => {
+    //     if(name1.length > 0){
+    //         filterData = name1.length === 0 ? filterDataOne : filterDataOne.filter((e) => e.name.toLowerCase().includes(name1.toLowerCase()))
+    //         // console.log("name fill ",name1.length ,filterData )
+    //     }
+    //     if(roll1.length > 0){
+    //         filterData = name1.length === 0 ? filterDataOne : filterDataOne.filter((e) => e.roll1.toLowerCase().includes(roll1.toLowerCase()))
+    //         // console.log("roll fill ", filterData)
+    //     }
+    //     if(contact1.length > 0){
+    //         filterData = name1.length === 0 ? filterDataOne : filterDataOne.filter((e) => e.phone.toLowerCase().includes(contact1.toLowerCase()))
+    //         // console.log("contact fill ", filterData)
+    //     }
+    
+    //     // console.log("filter name" , nameFilter)
+    // },[name1 , roll1 ,contact1 ])
+
+
   return (
     <div>
         <div>
         <table>
+
+<tr>
+    <td><input type="text"  onChange={(e) => setName1(e.target.value)} placeholder='Name'/></td>
+    <td><input type="text" value={roll1} onChange={(e) => setRoll1(e.target.value)}  placeholder='Roll no'/></td>
+    <td><input type="text" placeholder='Term'/></td>
+    <td><input type="text" placeholder='Year'/></td>
+    <td><input type="text" value={contact1} onChange={(e) => setContact1(e.target.value)} placeholder='Contact'/></td>
+
+</tr>
+
             <tr>
                 <th>Name</th>
                 <th>Roll no</th>
@@ -24,6 +67,10 @@ const[data , setData] = useState([])
                 <th>Contact Number</th>
             </tr>
 
+
+{
+
+}
 {
     filterData.map((e) => (
         <tr>
