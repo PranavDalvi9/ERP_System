043fd4edfd120af53a97306cdf25b4e0213cc8cb
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-06-07 13:01:09 +0530

    Login added

--- erp-system/src/Components/Login/Login.jsx
+++ erp-system/src/Components/Login/Login.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import axios from "axios"
 
 const Login = () => {
 
     const [userName, setUserName] = useState("")
     const[password , setPassword] = useState("")
+    const[userData , setUserData] = useState([])
 
     const handleAdd = () => {
         const data = {
@@ -12,6 +14,20 @@ const Login = () => {
         }
 
         console.log("data" , data)
+
+        axios.get("http://localhost:8080/student")
+        // .then((res) => { setUserData(res.data)
+
+        //   // const neww = res.data.filter((e) => e.email)
+        // const newArr = userData.filter((e) => e.email  === userName)
+        //   console.log("eee",newArr)
+        // })
+        .then((res) => {
+          const newArr = res.data.filter((e) => e.email ===userName )
+          console.log("eee" , newArr)
+        }
+        )
+        
     }
   return (
     <div>
