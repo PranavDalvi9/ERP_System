aff57e1a4f2b2a7a9eaf03c72ee837dddc74ed12
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-06-08 13:01:28 +0530

    Admin page updated

--- erp-system/src/App.js
+++ erp-system/src/App.js
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>hello</h1>
+      {/* <h1>hello</h1> */}
 
       {/* <Login/> */}
       {/* <Admin/> */}
--- erp-system/src/Components/StudentPortal/StudentPortal.jsx
+++ erp-system/src/Components/StudentPortal/StudentPortal.jsx
@@ -2,23 +2,27 @@ import React from 'react'
 import "./StudentPortal.css"
 import StudentInfo from "../StudentInfo/StudentInfo"
 import Evevts from '../Evevts/Evevts'
-import { useSelector } from 'react-redux'
 import ListStudents from '../ListStudents/ListStudents'
 import { useNavigate } from 'react-router-dom'
+import {useDispatch , useSelector} from "react-redux"
+import { logout } from '../../Redux/Login/Action'
 
 
 
 
 const StudentPortal = () => {
-const navigate = useNavigate()
-  const userData = useSelector((store) => store.loginUserData.userData[0][0].role)
-  // console.log("login stst", userData)
+
+const navigate = useNavigate();
+const dispatch = useDispatch();
+
+  const userData = useSelector((store) => store.loginUserData.userData[0][0])
+  console.log("login stst", userData)
 
   return (
     <div>
 
       {
-        userData === "Student" ? <>
+        userData.role === "Student" ? <>
           <div className='StudentPortal'>
             <div className='StudentInfo'>Student Information <StudentInfo /> </div>
             <div className='Events'><Evevts /> </div>
@@ -28,11 +32,16 @@ const navigate = useNavigate()
       }
 
       {
-        userData === "Admin" ? <>
+        userData.role === "Admin" ? <>
           <div>
-            <h1>hello admin</h1>
+            <h1>Hello Admin { userData.name}  </h1>
+            
             <button onClick={()=> navigate("/register_student") } >Add Student</button>
             <button onClick={()=> navigate("/event_register") } >Add Event</button>
+            <button onClick={() => {  dispatch(logout());
+  navigate("/login")}}>Logout</button>
+
+
             <div className='StudentList'>
               <ListStudents />
             </div>
